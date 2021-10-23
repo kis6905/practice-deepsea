d1b19e90f4fdff46c6dc72e45f102f3f7e6e67cc
kis6905 <kis6905@gmail.com>
2021-10-23 18:41:34 +0900

    init

---
+++ deepsea/src/main/java/com/deepsea/DeepseaApplication.java
@@ -0,0 +1,13 @@
+package com.deepsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepseaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepseaApplication.class, args);
+	}
+
+}
---
+++ deepsea/src/test/java/com/deepsea/DeepseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
